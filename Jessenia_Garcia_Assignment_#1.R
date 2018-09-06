1e182830b53f0021f7c41e7a587eba162997768f
Jessenia Garcia <42684790+jesseniagarcia@users.noreply.github.com>
2018-09-05 23:13:38 -0500

    Add files via upload

---
+++ Jessenia_Garcia_Assignment_#1.R
@@ -0,0 +1,47 @@
+x<-c(5,10,15,20,25,30)
+y<-c(-1,NA,75,3,5,8)
+z<-c(5)
+x*z
+y*z
+New1 (y*z)
+New1 <-(y*z)
+New2 <-(x*z)
+print (New1,New2)
+print (New1)
+print(New2)
+#replacing NA
+y<-ifelse(test = is.na(y)==T, yes = 2.5, no = y)
+print(y)
+#load data set
+prb<-read.csv""https://raw.githubusercontent.com/mattdemography/EDU_7043/master/Data/Assignment_1.csv" "
+print(state.abb)
+rm (print(state.abb))
+#print first 10 abb 
+print(state.abb, n = 10)
+head (state.abb, n = 10)
+#Mean Murder Rate
+mean (Assignment_1$Murder)
+#Median Murder Rate
+median (Assignment_1$Murder)
+#mean of NE
+mean (Assignment_1$Murder, n = NE)
+mean (Assignment_1$Murder, n = 29)
+mean(29 (Assignment_1$Murder))
+mean (Assignment_1$) [29, 3]
+mean (Assignment_1$Murder [29,3])
+mean (Assignment_1$df 29,
+Assignment_1<-apply(Assignment_1, [29, 3] mean)
+mean (Assignment_1$29)
+mean (Assignment_1$Murder, [29,3])
+NewEData <-subset(Assignment_1,State=="CT"| State=="NH"|State == "ME" | State == "MA" | State == "RI"| State == "VT")
+mean (NewEData$Murder)
+#Mean V Crime 
+mean (Assignment_1$Vcrime)
+mean
+#Save File 
+write.csv (Jessenia_Garcia_Assignment_1, file = )
+save (file,Jessenia_Garcia_Assignment_1)
+write.csv(x, file = "Jessenia_Garcia_Assignment_1
+write.csv(file="C:Jessenia_Garcia_Assignment_#1.Rdata)
+write.csv(prb, file="C:https://github.com/jesseniagarcia/EDU-7043.git
+          
